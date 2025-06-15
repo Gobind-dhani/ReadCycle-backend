4b2b828ee0d6e9e2512667df886564b1b3a2b0b2
gobind.barick <gobind.barick@dhani.com>
2025-06-15 19:20:58 +0530

    added sell_price for every book

--- src/main/java/com/readcycle/server/entity/Book.java
+++ src/main/java/com/readcycle/server/entity/Book.java
@@ -26,6 +26,7 @@ public class Book {
     private String title;
     private String author;
     private double price;
+    private Double sellPrice;
     private String condition;
     private String description;
     @Column(name = "image_url")
